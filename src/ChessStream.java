14f6ff0fade1f943a16ebc77bc5e4367afc731eb
Dew-OF-Aurora <admin@example.com>
2023-07-29 09:14:34 +0800

    1.21 优化输出

--- src/ChessStream.java
+++ src/ChessStream.java
@@ -13,99 +13,77 @@ import java.util.List;
  * @Description:
  */
 public class ChessStream {
-    static class Axis {
-        private int x = -1;
-        private int y = -1;
+    Axis TempInAxis = Axis.build(-1, -1);
+    String TempPath = null;
+    int ReadTimerFlag = -1;
 
-        public int getX() {
-            return x;
-        }
+    public int ReadFromInput(String FolderPath) {
 
-        public int getY() {
-            return y;
-        }
+        ReadTimerFlag = -1;
 
-        public List<Integer> getXY() {
-            List<Integer> list = new ArrayList<Integer>();
-            list.add(getX());
-            list.add(getY());
-            return list;
-        }
+        TempPath = FolderPath;
 
-
-        public Axis(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public static Axis build(int x, int y) {
-            return new Axis(x, y);
-        }
-
-        @Override
-        public String toString() {
-            return "Axis{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
-        }
-    }
-
-    Axis TempInAxis = Axis.build(-1,-1);
-
-    public Axis ReadFromInput(String FolderPath) {
         File folder = null;
-        if (!FolderPath.equals("")) {
-            folder = new File(FolderPath);
-        }
-        assert folder != null;
-        if (!folder.exists() && !folder.isDirectory()) {
-            System.out.println("The folder does not exist, please create a folder");
-            return null;
-        }
+        if (!FolderPath.equals("")) folder = new File(FolderPath);
+        else return 1;
+
+        if (!folder.exists() && !folder.isDirectory()) return 1;
+
         File[] files = folder.listFiles();// Obtain all files
         assert files != null;
         int FileCount = files.length;
-        if (FileCount != 1) {
-            System.out.println("Please check only one file in folder "+ FolderPath);
-            return null;
-        }
+
+        if (FileCount != 1) return 2;
         TempInAxis = Axis.build(Integer.parseInt(files[0].getName().substring(0, 2)), Integer.parseInt(files[0].getName().substring(2, 4)));
         // Del their file
         files[0].delete();
-
-        return TempInAxis;
+        return 0;
     }
 
     public Axis ReadTimer(String FolderPath) {
 
-        TempInAxis = Axis.build(-1,-1);
+        TempInAxis = Axis.build(-1, -1);
 
+        Thread Checker = new Thread(() -> {
+            synchronized (this){
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ignored) {
+                        // When Checker have be interrupted Unexpectedly
+                    }
+                    if (ReadTimerFlag == 1) {
+                        System.out.println("folder no exists");
+                        break;
+                    } else if (ReadTimerFlag == 2) {
+                        System.out.println("Waiting for opposite's reply,Please check only one file in folder " + FolderPath);
+                        break;
+                    }
+                    ReadTimerFlag = -1;
+                }
+            }
+        });
+        Checker.start();
         while (TempInAxis.getX() == -1 || TempInAxis.getY() == -1) {
-
-            System.out.println("Waiting for opposite's reply");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            ReadFromInput(FolderPath);
+            ReadTimerFlag = ReadFromInput(FolderPath);
         }
+        Checker.interrupt();
         return TempInAxis;
     }
 
     public void WriteMyMove(int a, int b, String FolderPath) {
+        String A;
+        String B;
         if (a == 0 || b == 0) return;
-        String A = "";
-        String B = "";
         if (a < 10 && a > 0) A = "0" + a;
-        else {
-            A = ""+ a;
-        }
+        else A = "" + a;
         if (b < 10 && b > 0) B = "0" + b;
-        else {
-            B = ""+ b;
-        }
+        else B = "" + b;
         String MoveInPath = FolderPath + "\\" + A + B;
         File MyMove = new File(MoveInPath);
         try {
@@ -124,6 +102,40 @@ public class ChessStream {
             throw new RuntimeException(e);
         }
         return s;
+    }
+
+    static class Axis {
+        private int x = -1;
+        private int y = -1;
+
+        public Axis(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Axis build(int x, int y) {
+            return new Axis(x, y);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public List<Integer> getXY() {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(getX());
+            list.add(getY());
+            return list;
+        }
+
+        @Override
+        public String toString() {
+            return "Axis{" + "x=" + x + ", y=" + y + '}';
+        }
     }
 
 }
